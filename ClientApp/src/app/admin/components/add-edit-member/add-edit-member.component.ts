7322defb33d02c54a0188a006ef9dbcb2ec6249e
Vladislav Petrosyan <petrosyanv@dynacare.ca>
2025-01-14 20:42:08 -0500

    added roles and validation for password

--- ClientApp/src/app/admin/components/add-edit-member/add-edit-member.component.ts
+++ ClientApp/src/app/admin/components/add-edit-member/add-edit-member.component.ts
@@ -63,33 +63,6 @@ export class AddEditMemberComponent implements OnInit, OnDestroy {
     }
   }
 
-  initializeForm(member: MemberAddEditInterface | null) {
-    this.formInitialized = true;
-    
-    if (member) {
-      this.memberForm = this.formBuilder.group({
-        id: [member.id],
-        firstName: [member.firstName, Validators.required],
-        lastName: [member.lastName, Validators.required],
-        userName: [member.userName, Validators.required],
-        password: [''],
-        roles: [member.roles, Validators.required],
-      });
-
-      this.existingMemberRoles = member.roles.split(',');
-      
-    } else {
-      this.memberForm = this.formBuilder.group({
-        id: [''],
-        firstName: ['', Validators.required],
-        lastName: ['', Validators.required],
-        userName: ['', Validators.required],
-        password: ['', Validators.required, Validators.minLength(8), Validators.maxLength(15)],
-        roles: ['', Validators.required],
-      });
-    }
-  }
-
   loadMemberAndRoles() {
     this.rolesSubscription = this.roles$.subscribe({
       next: (roles) => {
@@ -104,8 +77,64 @@ export class AddEditMemberComponent implements OnInit, OnDestroy {
     });
   }
 
-  submit(){
+  initializeForm(member: MemberAddEditInterface | null) {
+    this.formInitialized = true;
 
+    if (member) {
+      this.memberForm = this.formBuilder.group({
+        id: [member.id],
+        firstName: [member.firstName, Validators.required],
+        lastName: [member.lastName, Validators.required],
+        userName: [member.userName, Validators.required],
+        password: [''],
+        roles: [member.roles, Validators.required],
+      });
+      this.existingMemberRoles = member.roles.split(',');
+    } else {
+      this.memberForm = this.formBuilder.group({
+        id: [''],
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required],
+        userName: ['', Validators.required],
+        password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(15)]],
+        roles: ['', Validators.required],
+      });
+    }
+  }
+
+  passwordOnChange() {
+    if (!this.addNew) {
+      const password = (this.memberForm.get('password')?.value || '').trim();
+      if (password) {
+        this.memberForm.controls['password'].setValidators([
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(15),
+        ]);
+      } else {
+        this.memberForm.get('password')?.clearValidators();
+      }
+
+      this.memberForm.controls['password'].updateValueAndValidity();
+    }
+  }
+
+  roleOnChange(role: string) {
+    const roles = (this.memberForm.get('roles')?.value || '').trim().split(',');
+    const index = roles.indexOf(role);
+    index !== -1 ? roles.splice(index, 1) : roles.push(role);
+
+    if (!roles[0]) {
+      roles.splice(0, 1);
+    }
+
+    this.memberForm.controls['roles'].setValue(roles.join(','));
+  }
+
+  submit() {
+    this.submitted = true;
+    if (this.memberForm.valid) {
+    }
   }
 
   ngOnDestroy(): void {
